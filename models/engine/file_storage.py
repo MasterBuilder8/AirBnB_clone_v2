8ccb28eab3ece3a5b6827dae0e19c4d665f58dac
MasterBuilder8 <collins.snilloc9@gmail.com>
2023-12-20 08:56:48 -0800

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""This module is the file storage class for AirBnB clone"""
+
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -8,59 +9,71 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+import shlex
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
+    """This class serializes instances to a JSON file and
+    deserializes JSON file
+    Attributes:
+        __file_path: path to the JSON file
+        __objects: storage for objects
+        """
+    __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
         """Returns a dictionary of instantiated objects in __objects.
 
-        if a class(cls) is specified, the dictionary of the object is returned.
+        if a cls is specified, the dictionary of the object is returned.
         else, return the __objects dictionary
         """
-        if cls is not None:
-            if type(cls) == str:
-                cls = eval(cls)
-                cls_dict = {}
-                for k, v in self.__objects.items():
-                    if type(v) == cls:
-                        cls_dict[k] = v
-                        return cls_dict
+        dic = {}
+        if cls:
+            dictionary = self.__objects
+            for key in dictionary:
+                partition = key.replace('.', ' ')
+                partition = shlex.split(partition)
+                if (partition[0] == cls.__name__):
+                    dic[key] = self.__objects[key]
+                    return (dic)
+                else:
                     return self.__objects
 
     def new(self, obj):
-        """Set in __objects obj"""
-        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+        """Sets __objects to obj"""
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
-        """Saves storage dictionary to file"""
-        odict = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
-        with open(self.__file_path, "w", encoding="utf-8") as f:
-            json.dump(odict, f)
+        """Directs file path to JSON"""
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+            with open(self.__file_path, 'w', encoding="UTF-8") as f:
+                json.dump(my_dict, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Loads storage dictionary from file
+        """
         try:
-            with open(self.__file_path, "r", encoding="utf-8") as f:
-                for o in json.load(f).values():
-                    name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(name)(**o))
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, value in (json.load(f).items ():
+                        value = eval(value["__class__"])(**value)
+                        self.__objects[key] = value
         except FileNotFoundError:
             pass
 
         def delete(self, obj=None):
-            """Delete a given object from __objects, if it exists."""
-            try:
-                del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
-            except (AttributeError, KeyError):
-                pass
+        """delete an existing element
+        """
+        if obj:
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        del self.__objects[key]
 
             def close(self):
                 """reloads by using the reload method"""
